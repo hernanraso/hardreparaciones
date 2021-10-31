ddb4101195761d903227c2fe6024bb82b3746506
hernanraso <hernanraso@gmail.com>
2021-10-31 07:57:47 -0300

    master

--- form.js
+++ form.js
@@ -14,17 +14,30 @@ for (var i = 0; i < inputs.length; i++) {
 function validacion () {
     nom = document.getElementById("nombre").value;
     ape = document.getElementById("apellido").value;
-    if (nom == null || nom == 0 || ape == null || ape == 0) 
+    mail = document.getElementById("email").value;
+    tel = document.getElementById("telefono").value;
+    expresion = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/
+    if (nom == null || nom == 0 || ape == null || ape == 0 || mail == null || mail == 0 || tel == null || tel == 0) 
     {
-        document.getElementById("Mensaje").innerHTML = "El campo Nombre es Obligatorio";
-        document.getElementById("menape").innerHTML = "El campo Apellido es Obligatorio";
+        document.getElementById("Mensaje").innerHTML = "*El campo Nombre es Obligatorio";
+        document.getElementById("menape").innerHTML = "*El campo Apellido es Obligatorio";
+        document.getElementById("menmail").innerHTML = "*El campo Mail es Obligatorio";
+        document.getElementById("mentel").innerHTML = "*El campo Telefono es Obligatorio";
         elemento = document.getElementById("nombre");
         elemento = document.getElementById("apellido");
+        elemento = document.getElementById("email");
+        elemento = document.getElementById("telefono");
         elemento.focus();
         
         return false;
     }
 
-    return true;
+    else if(!expresion.test(email)) {
+        document.getElementById("menmail").innerHTML = "*El Correo no es valido";
+        elemento = document.getElementById("email");
+        return false;
+    }
+
+   
 
 }
